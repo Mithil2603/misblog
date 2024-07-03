1b6336a3fe00891421c40185dcdaa2372f4d7a5a
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-03 18:27:01 +0530

    iNotes title bug fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
         <>
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/home">iNotes</a>
+                    <Link className="navbar-brand" to="/home">iNotes</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
