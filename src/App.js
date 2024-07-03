4a9d11e6d33605a908efa231661e238e6324f4e0
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-03 18:13:31 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <Home></Home>
       <Router>
         <NavBar />
         <Routes>
