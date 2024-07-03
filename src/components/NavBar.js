cfd7e557e4578887653ae8284805e047b52c1c05
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-03 18:11:12 +0530

    bug fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
         <>
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/App.js">iNotes</a>
+                    <a className="navbar-brand" href="/">iNotes</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
