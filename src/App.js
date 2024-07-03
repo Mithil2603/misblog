e3242928ea7efbc1ac9240c82eec38b35c1da842
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-03 18:05:34 +0530

    Categories and NavBar Added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import BasicFunctionsManagement from './components/BasicFunctionsManagement';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Php from './components/Php';
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BasicFunctionsManagement></BasicFunctionsManagement>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/php" element={<Php />} />
+          <Route path="/basicfunctionsmanagement" element={<BasicFunctionsManagement />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
+
+createRoot(document.getElementById("root")).render(
+  <App />
+);
 
 export default App;
---
+++ src/components/Php.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Php() {
+  return (
+    <>
+        <div className="container">
+            <p>PHP</p>
+        </div>
+    </>
+  )
+}
