bcf75a0aec7a504f197179a4197114100eaa975a
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-04 19:39:49 +0530

    Home page now renders

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import NavBar from './components/NavBar';
 import Home from './components/Home';
 import Php from './components/Php';
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router basename="/">
-      <NavBar></NavBar>
+        <NavBar />
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/php" element={<Php />} />
           <Route path="/basicfunctionsmanagement" element={<BasicFunctionsManagement />} />
+          <Route path="*" element={<Navigate to="/" />} /> {/* Redirect any undefined routes to home */}
         </Routes>
       </Router>
     </div>
