aca1560e82147c02404de48c24f1e3823793c487
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-07-05 17:57:09 +0530

    button working now

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './Basic_Functions.css'
 import mis from './images/mis.png';
-import php from './images/PHP-logo.png'
+import php from './images/PHP-logo.png';
+import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
@@ -12,7 +13,7 @@ export default function Home() {
                     <div className="card-body">
                         <h5 className="card-title">Management Information System</h5>
                         <p className="card-text">A management information system is an information system used for decision-making, and for the coordination, control, analysis, and visualization of information in an organization. The study of the management information systems involves people, processes and technology in an organizational context.</p>
-                        <a href="#" className="btn btn-dark">View Notes</a>
+                        <Link to="/php"><a className="btn btn-dark">PHP</a></Link>
                     </div>
                 </div>
                 <div className="card text-center">
@@ -20,7 +21,8 @@ export default function Home() {
                     <div className="card-body">
                         <h5 className="card-title">PHP</h5>
                         <p className="card-text1">PHP is a general-purpose scripting language geared towards web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1993 and released in 1995. The PHP reference implementation is now produced by the PHP Group.</p>
-                        <a href="#" className="btn btn-dark">View Notes</a>
+                        {/* <a href="#" className="btn btn-dark">View Notes</a> */}
+                        <Link to="/basicfunctionsmanagement"><a href="#" className="btn btn-dark nostyle">View Notes</a></Link>
                     </div>
                 </div>
             </div>
