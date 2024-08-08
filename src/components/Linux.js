63035fc2d6fac20299cb144b3eecc4b80ceb7636
Mithil Suthar <136597187+Mithil2603@users.noreply.github.com>
2024-08-08 11:55:06 +0530

    Linux notes added

--- src/components/Linux.js
+++ src/components/Linux.js
@@ -10,10 +10,10 @@ export default function Linux() {
                     <hr />
                     <h2 className="secondary-title"><b>What is Linux?</b></h2>
                     <div className="description">
-                        <span className="text-primary">Linux</span> is an Operating System. Linux is a bit like Windows. It is a core software that runs your computer and lets you do stuff on it. <br></br><br />
-                        <span>There are two ways of looking at pc:</span>
-                        <li className='my-2'><b>Magical box</b></li>
-                        <div className="description1">
+                        <span className="text-primary"><b>Linux</b></span> is an Operating System. Linux is a bit like Windows. It is a core software that runs your computer and lets you do stuff on it. <br></br><br />
+                        <span className='my-2'><b>There are two ways of looking at pc:</b></span>
+                        <h5 className='my-4'><b>Magical box</b></h5>
+                        <div className="description1 my-2">
                             <ul className='text-indent my-2'>
                                 <li>It allows you to browse internet, play games etc.</li>
                                 <li>Type address of a website and website will appear on your screen.</li>
@@ -23,10 +23,10 @@ export default function Linux() {
                                 <li>Microsoft maintains its wealth by a virtual monopoly over PC manufacturers.</li>
                                 <li>A computer buyer has choice to select a Hardware component among various hardware components of different manufacturers.</li>
                                 <li>But a computer buyer has limited choice to select an operating system
-                                at the time of PC purchase.</li>
+                                    at the time of PC purchase.</li>
                             </ul>
                         </div>
-                        <li className='my-2'><b>Collection of components made by various manufacturers</b></li>
+                        <h5 className='my-4'><b>Collection of components made by various manufacturers</b></h5>
                         <div className="description1">
                             <ul className='text-indent my-2'>
                                 <li>PC consists of a CPU, hard disk, graphics card and so on.</li>
@@ -98,6 +98,120 @@ export default function Linux() {
                         </p>
                     </div>
                     <hr />
+                    <h3 className="third-title"><b>The Benefits of Linux</b></h3>
+                    <div className="description1">
+                        <p className="text">
+                            <ol>
+                                <h5 className='mt-0 pt-0'><li>Cash Free</li></h5>
+                                <ul className='mt-3'>
+                                    <li>It doesn't crash.</li>
+                                    <li>Mouse cursor never freeze on screen like windows.</li>
+                                    <li>A strange error box won't appear and not go away until you reboot.</li>
+                                    <li>It's possible to leave a Linux system running for years without ever needing to reboot.
+                                    </li>
+                                    <li>Programs that run on the Linux sometimes crash, but they don't take the system down with them, as it happens in windows.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Security</li></h5>
+                                <ul className='mt-3'>
+                                    <li>Linux is more secured than windows.</li>
+                                    <li>Linux works on the principle of users who have permissions to undertake various tasks on the system. If you don't have the correct permission, then you cannot, for example, access a particular piece of hardware.</li>
+                                    <li> privacy can be ensured because the files on the PC are “owned” by individual users, who can permit or deny others access to those files.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Free and Shareable</li></h5>
+                                <ul className='mt-3'>
+                                    <li>Linux can be obtained free of charge.</li>
+                                    <li>The latest updates of all your programs are free of
+                                    charge.</li>
+                                    <li>if you want any new software, it will also usually be free of charge.</li>
+                                    <li>Because software is free, you can share it with your friend. For ex: image editor</li>
+                                    <li> Under Windows, copying the program is strictly illegal, to do so turns you into a software pirate. Unless that image editor is freeware, your friend will need to buy the software himself.</li>
+                                    <li>Under Linux, sharing software is normally entirely legal.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Compatible with Older Hardware</li></h5>
+                                <ul className='mt-3'>
+                                    <li>Linux works very well on older hardware and doesn't require an Advanced PC system.</li>
+                                    <li>In case of windows, some times upgradation of operating system requires to upgrade entire computer or buying a new computer, even if old computer is working fine.</li>
+                                    <li>Linux works fine on the computers of 1990s.</li>
+                                    <li>Linux encourages an attitude of both recycling and making the most of what you have, rather than constantly upgrading and buying a new hardware.</li>
+                                    <li>“old pc”-Install Linux- gift to some one in family or kids or friends who don't have pc.</li>
+                                    <li>Turn old hardware into server.</li>
+                                    <li>“Old PC”-{">"}Turn-{">"}web server, e-mail server, or firewall</li>
+                                    <li>In case of windows, for the same kind of conversion it requires of hundreds of dollars and new advanced computer system.</li>
+                                </ul>
+                            </ol>
+                        </p>
+                    </div>
+                    <hr />
+                    <h3 className="third-title"><b>Features of Unix</b></h3>
+                    <div className="description1">
+                        <p className="text">
+                            <ol>
+                                <h5 className='mt-0 pt-0'><li>Unix: A Multiuser system</li></h5>
+                                <ul className='mt-3'>
+                                    <li>Unix is a multiprogramming system, it permits multiple programs to run and compete for the attention of the CPU.</li>
+                                    <li>It can happen in two ways:
+                                        <ul className='mx-3'>
+                                            <li>Multiple users can run separate jobs</li>
+                                            <li>A single user can also run multiple jobs</li>
+                                        </ul>
+                                    </li>
+                                    <li>Windows is a single user system where CPU, Memory and hard disk are dedicated to a single user.</li>
+                                    <li>In Unix the resources are shared between all users; Unix is a multiuser system.</li>
+                                    <li>Computer divides a unit of time into several segments and each user is allocated a segment, so at a time machine will be doing the job of a single user only.</li>
+                                    <li>Once the allocated time expires the previous job will be suspended and the next user's job will be taken.</li>
+                                    <li>It will continue until the clock has turned full circle and the first user's job is taken again.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Unix: A Multitasking system Too</li></h5>
+                                <ul className='mt-3'>
+                                    <li>A single user can also run multiple tasks concurrently; Unix is a multitasking system.</li>
+                                    <li>It is usual for a user to edit a file, print another one on printer, send email and browse the internet all without leaving any of the applications.</li>
+                                    <li>The Kernel is designed to handle user's multiple needs.</li>
+                                    <li>In a multitasking environment, a user sees one job running in the foreground and the rest run in the background. User can switch jobs between foreground, background , suspend or terminate them.</li>
+                                    <li>It will continue until the clock has turned full circle and the first user's job is taken again.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>The Building-Block approach</li></h5>
+                                <ul className='mt-3'>
+                                    <li>The designers have never attempted to pack too many features into a few tools, instead developed a few hundred commands each of which performed one simple job only.</li>
+                                    <li>Ex: ls | wc</li>
+                                    <li>The above example will count the number of files and subdirectories in the current directory. No separate command is formed to perform this task.</li>
+                                    <li> The commands that can be connected in this way re called filters because they filter or manipulate data in different ways.</li>
+                                    <li>Today many Unix tools are designed with the requirement that the output of one tool can be used as input to another.</li>
+                                    <li>By interconnecting a number of tools, user can have a large number of combinations of their usage.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>The Unix Toolkit</li></h5>
+                                <ul className='mt-3'>
+                                    <li> Unix represents the kernel but the kernel itself doesn't do much that can benefit the user.</li>
+                                    <li>There are general-purpose tools, text manipulation utilities(called filters), compilers and interpreters, networked applications and system administration tools also.</li>
+                                    <li>User can select one from variety of shells.</li>
+                                    <li>New tools are being added and the older ones are being removed or modified with every Unix release.</li>
+                                    <li>The open-source versions for most of the utilities are available.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Pattern Matching</li></h5>
+                                <ul className='mt-3'>
+                                    <li>Unix has very sophisticated pattern matching features.</li>
+                                    <li>Ex: ls chap*</li>
+                                    <li>The above example will list out all the filenames which are starting with “chap” word.</li>
+                                    <li>Here the *(metacharacter) is a special character used by the system to indicate that it can match a number of filenames.</li>
+                                    <li>Unix has also a special expression called a regular expression that is framed with characters from the set of metacharacters, which can perform complex pattern matching tasks also.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Programming Facility</li></h5>
+                                <ul className='mt-3'>
+                                    <li>The Unix shell is also a programming language which was designed for a programmer not for a casual end user.</li>
+                                    <li>It has control structures, loops and variables which establishes it as a powerful programming language.</li>
+                                    <li>These features are used to design shell scripts.</li>
+                                    <li>Many of the system's functions can be controlled and automated by using shell scripts.</li>
+                                </ul>
+                                <h5 className='mt-4'><li>Documentation</li></h5>
+                                <ul className='mt-3'>
+                                    <li>The principal online help facility available is the man command, which remains the most important reference for commands and their configuration files.</li>
+                                    <li>A vast ocean of Unix resources are available on the internet.</li>
+                                    <li>There exists several newsgroups on Unix where an user can fire his/her query and can get solution from the newsgroup community members.</li>
+                                    <li>The FAQ is a document that addresses common problems and is also widely available on the internet.</li>
+                                    <li>There are numerous articles published in magazines and journals and lecture notes made available by universities on their websites.</li>
+                                </ul>
+                            </ol>
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
